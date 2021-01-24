5604b4326256d8ad629c2a4c36cbf1e792c3b426
Gabriele Losi <gabriele.losi@outlook.com>
2021-01-24 12:29:40 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from setuptools import setup
 
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
 setup(
     name='physis',
-    version=0.0.0,
-    description='Modules for physical and chemical sciences.'
-    long_description=open('README.md').read(),
-    url='https://github.com/glosi000/physis'
-    author='Gabriele Losi'
+    version='0.0.0',
+    description='Modules for physical and chemical sciences.',
+    long_description=long_description,
+    url='https://github.com/glosi000/physis',
+    author='Gabriele Losi',
     author_email='gabriele.losi@outlook.com',
-    maintainer='Gabriele Losi'
+    maintainer='Gabriele Losi',
     maintainer_email='gabriele.losi@outlook.com',
     license='BSD-4-Clause License',
-    install_requires=['numpy', 'matplotlib', 'numba']
+    install_requires=['numpy', 'matplotlib', 'numba'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD-4-Clause License",
+        "Operating System :: OS Independent",
+    ]
 )
