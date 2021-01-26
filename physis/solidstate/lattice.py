3827dbc1d136641047709168e7936d4593145be2
Gabriele Losi <gabriele.losi@outlook.com>
2021-01-26 14:59:26 +0100

    Add a crystal class, improve lattice

--- physis/solidstate/lattice.py
+++ physis/solidstate/lattice.py
@@ -3,7 +3,7 @@
 """
 Created on Mon Jan 25 15:57:41 2021
 
-Basic tools for solid state physics.
+Classes and functions to work with lattices.
 
 @author: glosi000
 """
@@ -20,34 +20,44 @@ __date__ = 'January 25th, 2021'
 import numpy as np
 
 
-class CrystalError(Exception): 
-    """ General class for errors in crystalline structures
-    """
-    pass
-
-class LatticeError(CrystalError):
+class LatticeError(Exception):
     """ General class for errors in crystal lattices
     """
     pass
 
+
 class InputLatticeError(LatticeError):
-    
+    """ Manage input errors in building lattices
+    """
+
     @staticmethod
     def cubic(alat, replica):
-        
         # Check input parameters
         if not isinstance(replica, (list, tuple, np.ndarray)):
-            raise InputLatticeError('Argument of the wrong type: replica')
-        elif not isinstance(alat, (int, float)):
-            raise InputLatticeError('Argument of the wrong type: alat')
-        elif any (i <= 0 for i in replica):
-            raise InputLatticeError('Invalid Argument: replica cannot be <= 0')            
+            raise InputLatticeError('Wrong type: replica')
+        if any(i <= 0 for i in replica):
+            raise InputLatticeError('Invalid Argument: replica')
+        if not isinstance(alat, (int, float)):
+            raise InputLatticeError('Wrong type: alat')
 
-class Lattice:
-    """ Generate a crystalline lattice of points.
-    TODO : Add a crystal class that is capable to insert a basis
+
+class Cell:
+    """ Simple class to store the information about the unit cell
     """
     
+    def __init__(self, cell):
+        self.cell = cell   
+    
+    @classmethod
+    def create_cube(cls, alat):
+        return cls(cell = np.diag(np.full(3, alat)))
+    
+
+class Lattice(Cell):
+    """ Generate a crystalline lattice of points.
+    """
+
+    # Conventional lattice sites
     sc_sites  = np.array([0, 0, 0])
     bcc_sites = np.array([[0, 0, 0],
                           [0.5, 0.5, 0.5]])
@@ -56,74 +66,74 @@ class Lattice:
                           [0.5, 0, 0.5],
                           [0, 0.5, 0.5]])
     
-    def __init__(self):
-        pass
+    def __init__(self, cell, lattice):
+        self.cell = cell
+        self.lattice = lattice
+
+    @property
+    def latx(self):
+        return self.lattice[:, 0]
     
-    @staticmethod
-    def generate_cubic_cell(alat, sites, replica=(1,1,1), perturbation=False, 
-                            delta=1e-8):
-                
+    @property
+    def laty(self):
+        return self.lattice[:, 1]
+    
+    @property
+    def latz(self):
+        return self.lattice[:, 2]
+
+    
+    def generate_cubic_lattice(alat, sites, replica=(1,1,1), 
+                               perturbation=False, delta=1e-8):
+
         # Check inputs and get basic information
         InputLatticeError.cubic(alat, replica)
 
         # Build the lattice
-        lattice_sites = alat * Lattice.build_sites(sites, replica)
-        
+        lat_sites = alat * Lattice.build_sites(sites, replica)
+
+        # Insert perturbation
         if perturbation:
             N = np.prod(replica, dtype=int) * sites.shape[0]
-            lattice_sites += np.random.normal(0., delta, size=(N, 3))
+            lat_sites += np.random.normal(0., delta, size=(N, 3))
         
-        return lattice_sites
-
-    @staticmethod
-    def generate_sc(alat, replica=(1,1,1), perturbation=False, delta=1e-8):
-        
-        lattice_sites = Lattice.generate_cubic_cell(alat, Lattice.sc_sites, 
-                                                    replica, perturbation, delta)
-        
-        return lattice_sites
-
-    @staticmethod
-    def generate_bcc(alat, replica=(1,1,1), perturbation=False, delta=1e-8):
-        
-        lattice_sites = Lattice.generate_cubic_cell(alat, Lattice.bcc_sites, 
-                                                    replica, perturbation, delta)
-        
-        return lattice_sites
-
-    @staticmethod
-    def generate_fcc(alat, replica=(1,1,1), perturbation=False, delta=1e-8):
-        
-        lattice_sites = Lattice.generate_cubic_cell(alat, Lattice.fcc_sites, 
-                                                    replica, perturbation, delta)
-
-        return lattice_sites
+        return lat_sites
     
-    @staticmethod
-    def generate_diamond(alat, replica=(1,1,1), basis=None, perturbation=False,
-                     delta=1e-8):
-        
-        basis = alat * np.array([0, 0 ,0],
-                                [0.25, 0.25, 0.25])
-        lattice_sites =  Lattice.generate_cubic_cell(alat, basis, replica, 
-                                                     perturbation, delta)
-        
-        return lattice_sites
-
-    @staticmethod
     def build_sites(sites, replica=(1, 1, 1)):
-        
+
         # Calculate the needed number of points and prepare output array
-        N = np.prod(replica, dtype=int) * sites.shape[0]
-        lattice_sites = np.zeros(shape=(N, 3))
-        
+        N = np.prod(np.array(replica), dtype=int) * sites.shape[0]
+        lat_sites = np.zeros(shape=(N, 3))
+
         # Loop and replicate the original sites
         j = 0
         for n in range(replica[0]):
             for m in range(replica[1]):
                 for l in range(replica[2]):
+
                     # Calculate the repetition units for the lattice sites
-                    lattice_sites[j:j+4,:] = sites + np.array([n, m, l])
+                    lat_sites[j:j+4,:] = sites + np.array([n, m, l])
                     j += 4
-            
-        return lattice_sites
+        return lat_sites
+
+    @classmethod
+    def generate_sc(cls, alat, replica=(1,1,1), perturbation=False, delta=1e-8):
+
+        lat_sites = Lattice.generate_cubic_lattice(alat, Lattice.sc_sites, 
+                                                   replica, perturbation, delta)
+        return cls(cell = np.diag(np.full(3, alat)), lattice = lat_sites)
+
+    @classmethod
+    def generate_bcc(cls, alat, replica=(1,1,1), perturbation=False, delta=1e-8):
+
+        lat_sites = Lattice.generate_cubic_lattice(alat, Lattice.bcc_sites, 
+                                                   replica, perturbation, delta)
+        return cls(cell = np.diag(np.full(3, alat)), lattice = lat_sites)
+
+    @classmethod
+    def generate_fcc(cls, alat, replica=(1,1,1), perturbation=False, delta=1e-8):
+
+        lat_sites = Lattice.generate_cubic_lattice(alat, Lattice.fcc_sites, 
+                                                   replica, perturbation, delta)
+        return cls(cell = np.diag(np.full(3, alat)), lattice = lat_sites)
+
